1c98fa183b97cc83fa09e457a9c4ab09ded2f47c
Assan SANOGO <datavizard.pharma@gmail.com>
2016-11-28 16:03:47 +0100

    Add files via upload
    Mapreduce.cs allows to take a text as input (here Lorem Ipsum) and run the map reduce algorithm.
    The input file is a text file, the output is also a text file.
    
    The Open a File.cs is just  a snippet.

---
+++ Mapreduce.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Open_a_text_File
+{
+    class Test
+    {
+        //Mapper 
+        //1.Cleaner
+        public String cleanText(string input)
+        {
+            var fixedInput = Regex.Replace(input, "[^a-zA-Z .]", " ");
+            String newInput = fixedInput.Replace("\"", "").Replace(".", " ");
+            return (newInput);
+        }
+
+        //2.Splitter
+        public String[] splitText(char splitter, string input)
+        {
+            String[] Words = input.Split(splitter);
+            return (Words);
+        }
+
+        //3. Sorter
+        public String[] sortText(String[] text_array)
+        {
+            Array.Sort(text_array);
+            return (text_array);
+        }
+
+        //4. Saver to disk
+        public void display_l(string[] input_array)
+        {
+            String[] str = { ".", "," };
+
+            // this is the mapped+sorted file before redcution - please feel free to modify Be careful if you modify here(+)
+            //you must modify below - when the file is called
+
+            using (StreamWriter outputFile = new StreamWriter(@"C:\Users\vc2.sophia\Downloads\Assan tutos\WriteLines.txt"))
+            {
+                foreach (string w in input_array)
+                {
+                    if ((w.Split(',')[0] != str[0]) || (w.Split(',')[0] != str[1]) || (String.IsNullOrEmpty(w.Split(',')[0])))
+                    {
+                        outputFile.WriteLine(w + ",1");
+                    }
+                }
+            }
+        }
+
+        //5.Reducer part
+        public void read_l(StreamReader s)
+        {
+            int my_total = 0;
+            String my_oldkey = "Welcome to Map reduce in C# !!!";
+            String my_text = "";
+
+            //this is the destination file - please feel free to modify
+            using (StreamWriter mpr = new StreamWriter(@"C:\Users\vc2.sophia\Downloads\Assan tutos\Mapreduced.txt"))
+            {
+                // this is the mapped+sorted file before redcution - please feel free to modify(++)
+                using (StreamReader s_i = new StreamReader(@"C:\Users\vc2.sophia\Downloads\Assan tutos\WriteLines.txt"))
+                {
+                    while (s_i.Peek() >= 0)
+                    {
+                        //Console.WriteLine(s_i.ReadLine());
+                        {
+                            //Console.WriteLine(sr.ReadLine());
+
+                            my_text = s_i.ReadLine().Split(',')[0];
+                            int anum = 0;
+                            Int32.TryParse((s_i.ReadLine().Split(',')[1]), out anum);
+
+                            if (my_text != my_oldkey)
+                            {
+                                Console.WriteLine(my_oldkey + "," + my_total);
+                                mpr.WriteLine(my_oldkey + "," + my_total);
+
+                                my_total = 0;
+                                my_oldkey = my_text;
+                            }
+
+                            my_oldkey = my_text;
+                            my_total = my_total + anum;
+                        }
+                    }
+                    Console.WriteLine(my_oldkey + "," + my_total);
+                    mpr.WriteLine(my_oldkey + "," + my_total);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Test t = new Test();
+                using (StreamReader str = new StreamReader(@"C:\Users\vc2.sophia\Downloads\Assan tutos\Mytext2.txt"))
+                {
+                    String line = str.ReadToEnd();
+                    String line2 = t.cleanText(line);
+                    String[] words = t.splitText(' ', line2);
+
+                    t.sortText(words);
+                    t.display_l(words);
+                }
+                StreamReader sr = new StreamReader(@"C:\Users\vc2.sophia\Downloads\Assan tutos\WriteLines.txt");
+                t.read_l(sr);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("the file could not be read.");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Open_a_File.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace OpenFiles
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            //the @ is used in order not to have to escape weird characters
+            string filePath = @"C:\Users\vc2.sophia\Downloads\Mytext.txt";
+
+            //if the file already exists, we delete it
+            if (File.Exists(filePath))
+                {
+                File.Delete(filePath);
+            }
+            //create the file
+            using (FileStream fs = File.Create(filePath))
+            {
+                //We create an array in which we put the bytes of the string info
+                Byte[] info = new UTF8Encoding(true).GetBytes("This is a weird way to write to a file ");
+               
+                //we write in the filestream the text represented by info starting at char zero and ending at the end of the string
+                fs.Write(info, 0, info.Length);
+            }
+            //read the file
+            using (FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                //create an empty array
+                byte[] b = new byte[1024];
+
+                //create an encoder object
+                UTF8Encoding temp = new UTF8Encoding(true);
+                
+                //read the stream until exhaustion of the bytes and print them in the Console
+                while(fs.Read(b,0,b.Length)>0)
+                {
+                    Console.WriteLine(temp.GetString(b));
+                } 
+            }
+        }
+    }
+}
